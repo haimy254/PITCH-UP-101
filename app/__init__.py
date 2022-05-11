b41bf27e5e6e68078fdafef1d993030c8df20e20
haimy254 <haimanaare@gmail.com>
2022-05-11 07:26:41 +0300

    initial commit in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-app= Flask (__name__)
-bootstrap = Bootstrap()
-db = SQLAlchemy()
+def create_app(config_name):
+    app.config.from_object(config_options[config_name])
+    
+    app= Flask (__name__)
+    bootstrap = Bootstrap(app)
+    db = SQLAlchemy()
 
 
     # Initializing flask extensions
-bootstrap.init_app(app)
-db.init_app(app)
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    return app
