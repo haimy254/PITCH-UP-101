e6d4c1911dcf6e5b4e7ba8f3c1ec454dbe58c244
haimy254 <haimanaare@gmail.com>
2022-05-12 14:26:42 +0300

    updated main

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
 from ..models import User
-from flask import render_template,abort
+from flask import render_template,abort, url_for
 from app import main
+from .forms import ReviewForm,UpdateProfile
+from .. import db
+from flask_login import login_required, redirect
+
 
 @main.route('/user/<uname>')
 def profile(uname):
@@ -9,5 +13,25 @@ def profile(uname):
         abort(404)
          
     return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
     
 
