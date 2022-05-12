4a7f67da0d69e08b21a3c70fe744de4655b121d6
haimy254 <haimanaare@gmail.com>
2022-05-12 08:19:27 +0300

    password verification added in test signup

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from wekzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 
-@login_manager.user_loader
+@login_manager.uqer_loader
 def load_user(user_id):
         return User.query.get(int(user_id))
 class User(db.model):
@@ -18,7 +18,6 @@ class User(db.model):
     
     def __repr__(self):
         return f'User{self.username}'
-    
     @property
     def password(self):
       raise AttributeError('this is restriced')
