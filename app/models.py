66dfc2ad1184cc06a822b0dc6bb09c29ff548216
haimy254 <haimanaare@gmail.com>
2022-05-12 07:52:20 +0300

    models

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from wekzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 
-@login_manager.uer_loader
+@login_manager.user_loader
 def load_user(user_id):
         return User.query.get(int(user_id))
 class User(db.model):
@@ -18,6 +18,7 @@ class User(db.model):
     
     def __repr__(self):
         return f'User{self.username}'
+    
     @property
     def password(self):
       raise AttributeError('this is restriced')
